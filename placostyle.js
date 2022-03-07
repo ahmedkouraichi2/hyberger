0897ad2927826502c75da986943def04ebc12a98
ahmedkouraichi2 <kouraichiahmed2@gmail.com>
2022-03-07 13:57:38 +0100

    init

---
+++ placostyle.js
@@ -0,0 +1,21 @@
+function verifNomPrenomPasValides1t(){
+    var r0 = verifChampNomVidesVersion0('fromExNom');
+    var r1 = verifChampNomVidesVersion1('fromEx1prenom');
+    var msg0 ="";
+    var msg1 ="";
+    if(! r0) 
+          msg0 = "champ vide ";
+
+    if(! r1)
+        msg1="champ vide ";
+
+    
+        document.getElementById('')
+
+
+
+
+
+
+
+}
